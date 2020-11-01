db5f89273e81352d615448e8c76f46a50be68ed4
nxqq <58228327+6lyxt@users.noreply.github.com>
2020-11-01 15:51:08 +0100

    bug fixes

--- src/at/dietze/dior/Dior.java
+++ src/at/dietze/dior/Dior.java
@@ -13,7 +13,8 @@ public class Dior {
 
     /* Main Method */
     public static void main(String[] args){
-        new Dior().initFrame();
+        Dior dior = new Dior();
+        dior.initFrame();
     }
 
     /* Mainframe construction */
